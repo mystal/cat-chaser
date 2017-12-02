600fbf1f979a5eaa344cad75784254b8a29432f1
Morgan Tenney <morgantenney@Morgans-MacBook-Pro.local>
2017-12-02 15:48:01 -0800

    Basic cat fleeing and bounding. Win condition doesn't work yet :(

--- src/world.rs
+++ src/world.rs
@@ -1,6 +1,6 @@
 use midgar::Midgar;
 
-use cgmath::{self, Vector2};
+use cgmath::{self, Vector2, InnerSpace};
 use midgar::{self, KeyCode};
 
 use config;
@@ -15,8 +15,79 @@ pub struct Dog {
     pub down_key: KeyCode,
 }
 
+pub enum CatType {
+    Basic,
+}
+
+pub enum CatState {
+    Flee,
+    Idle,
+    InPen
+}
+
 pub struct Cat {
     pub pos: Vector2<f32>,
+    pub cat_type: CatType,
+    pub radius: f32,
+    pub speed: f32,
+    pub size: Vector2<f32>,
+}
+
+impl Cat {
+    fn get_state(&self, dog: &Dog, cat_box: &CatBox) -> CatState {
+        let dog_to_cat = self.pos - dog.pos;
+
+        match &self.cat_type {
+            _ => { },
+        }
+
+        if cat_box.in_bounds(&self.pos) {
+            return CatState::InPen;
+        }
+        if dog_to_cat.magnitude() < self.radius {
+            return CatState::Flee;
+        }
+
+        CatState::Idle
+    }
+
+    fn flee(&mut self, bounds: &Vector2<f32>, dir: &Vector2<f32>) {
+        match &self.cat_type {
+            _ => { },
+        }
+
+        let speed = self.speed;
+        self.try_move(bounds, dir.normalize() * speed);
+    }
+
+    fn idle(&mut self, bounds: &Vector2<f32>) {
+
+    }
+
+    fn in_pen(&mut self, bounds: &Vector2<f32>) {
+        // TODO: wander in random direction
+        // self.pos = self.pos;
+    }
+
+    fn try_move(&mut self, bounds: &Vector2<f32>, change: Vector2<f32>) {
+        let br = self.pos + self.size;
+
+        if br.x + change.x > bounds.x {
+            self.pos.x = bounds.x - self.size.x;
+        } else if self.pos.x + change.x < 0.0 {
+            self.pos.x = 0.0;
+        } else {
+            self.pos.x = self.pos.x + change.x;
+        }
+
+        if br.y + change.y > bounds.y {
+            self.pos.y = bounds.y - self.size.y;
+        } else if self.pos.y + change.y < 0.0 {
+            self.pos.y = 0.0;
+        } else {
+            self.pos.y = self.pos.y + change.y;
+        }
+    }
 }
 
 pub struct CatBox {
@@ -24,11 +95,18 @@ pub struct CatBox {
     pub size: Vector2<f32>,
 }
 
+impl CatBox {
+    pub fn in_bounds(&self, point: &Vector2<f32>) -> bool {
+        let br = self.pos + self.size;
+        point.x >= self.pos.x && point.x <= br.x && point.y >= br.y && point.y <= self.pos.y
+    }
+}
+
 pub struct GameWorld {
     pub dog: Dog,
     pub cats: Vec<Cat>,
     pub cat_box: CatBox,
-    pub bounds: Vector2<u32>,
+    pub bounds: Vector2<f32>,
 }
 
 impl GameWorld {
@@ -36,6 +114,10 @@ impl GameWorld {
         let cats = vec![
             Cat {
                 pos: cgmath::vec2(400.0, 400.0),
+                cat_type: CatType::Basic,
+                radius: 20.0,
+                speed: 1.0,
+                size: cgmath::vec2(30.0, 30.0),
             },
         ];
 
@@ -52,7 +134,7 @@ impl GameWorld {
                 pos: cgmath::vec2(200.0, 200.0),
                 size: cgmath::vec2(80.0, 80.0),
             },
-            bounds: config::SCREEN_SIZE,
+            bounds: cgmath::vec2(config::SCREEN_SIZE.x as f32, config::SCREEN_SIZE.y as f32),
         }
     }
 
@@ -75,7 +157,29 @@ impl GameWorld {
         }
 
         // TODO: Cats move or run!
+        for cat in &mut self.cats {
+            match cat.get_state(&self.dog, &self.cat_box) {
+                CatState::Idle => { cat.idle(&self.bounds) },
+                CatState::InPen => { cat.in_pen(&self.bounds) },
+                CatState::Flee => {
+                    let dir = &cat.pos - self.dog.pos;
+                    cat.flee(&self.bounds, &dir)
+                },
+                _ => {},
+            }
+        }
 
         // TODO: Check win condition!
+        let mut we_win = true;
+        for cat in &self.cats {
+            if !self.cat_box.in_bounds(&cat.pos) {
+                we_win = false;
+                break;
+            }
+        }
+
+        if we_win {
+            println!("YOU WON");
+        }
     }
 }
