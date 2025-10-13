071a456734ea19be16a9765b0f9f855b388c7fc4
Gabriel Martinez <reitaka@gmail.com>
2025-10-13 13:12:51 -0700

    Get the crate name from module_path for logging
    To aovid hard-coding the name.

--- src/log.rs
+++ src/log.rs
@@ -1,14 +1,18 @@
 use bevy::log::{Level, LogPlugin};
 
 pub fn log_plugin() -> LogPlugin {
+    // Get the crate name to configure logging.
+    let (crate_name, _) = module_path!()
+        .split_once("::")
+        .unwrap_or((module_path!(), ""));
+
     // Configure logging.
     let mut plugin = LogPlugin::default();
-    // TODO: Is there a way to get the crate name automatically?
     if cfg!(feature = "verbose_logs") {
-        plugin.filter.push_str(",info,cat_chaser=trace");
+        plugin.filter.push_str(&format!(",info,{crate_name}=trace"));
         plugin.level = Level::TRACE;
     } else if cfg!(debug_assertions) {
-        plugin.filter.push_str(",info,cat_chaser=debug");
+        plugin.filter.push_str(&format!(",info,{crate_name}=debug"));
         plugin.level = Level::DEBUG;
     }
     plugin
